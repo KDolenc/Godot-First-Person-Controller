842fc868c330a294d98dc9b1aebe88949d2a1d80
KDolenc <kiel.d1999@gmail.com>
2024-10-05 00:23:35 +1000

    Update walking_player_state.gd

--- first_person_controller/state_machine/states/walking_player_state.gd
+++ first_person_controller/state_machine/states/walking_player_state.gd
@@ -25,7 +25,7 @@ func update(delta: float) -> void:
 		transition.emit("RunningPlayerState")
 	
 	# Transition to JumpState if jump input detected.
-	elif Input.is_action_just_pressed("jump"):
+	elif Input.is_action_pressed("jump"):
 		transition.emit("JumpingPlayerState")
 	
 	# Transition to CrouchState if crouch input detected.
